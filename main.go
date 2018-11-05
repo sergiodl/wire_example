c89155ff057496af51da952f9cd0bf81d8090ad2
Sérgio Lemos <sergio.dl@hotmail.com>
2018-11-04 18:14:16 -0800

    Added initial files

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Message string
+
+type Greeter struct {
+	Message
+}
+
+type Event struct {
+	Greeter
+}
+
+func NewMessage() Message {
+	return Message("Hi there!")
+}
+
+func NewGreeter(m Message) Greeter {
+	return Greeter{Message: m}
+}
+
+func (g Greeter) Greet() Message {
+	return g.Message
+}
+
+func NewEvent(g Greeter) Event {
+	return Event{Greeter: g}
+}
+
+func (e Event) Start() {
+	msg := e.Greeter.Greet()
+	fmt.Println(msg)
+}
+
+func main() {
+	event := InitializeEvent()
+	event.Start()
+}
---
+++ wire.go
@@ -0,0 +1,12 @@
+//+build wireinject
+
+package main
+
+import (
+	"github.com/google/go-cloud/wire"
+)
+
+func InitializeEvent() Event {
+	wire.Build(NewEvent, NewGreeter, NewMessage)
+	return Event{}
+}
